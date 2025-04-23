e627762459ad037f9eee952027032fdb87bd69e6
romtyui <145352092+romtyui@users.noreply.github.com>
2025-04-24 00:40:43 +0800

    形狀/顏色 代碼更新

--- Assets/Object_create_code.cs
+++ Assets/Object_create_code.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Object_create_code : MonoBehaviour
 {
@@ -9,7 +11,14 @@ public class Object_create_code : MonoBehaviour
     private string Data_name , OGData_name;
     public GameObject[] create_Objs;
     public GameObject create_pOS;
+
+    public enum OrderStatus { Obstacle/*障礙物*/, Bridge/*橋*/, Null/*不要動作*/ };
+    [Header("生成物件狀態")]
+    public OrderStatus status;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
     void Start()
     {
         IRS = GetComponent<Image_Recognition_script>();
@@ -19,6 +28,7 @@ public class Object_create_code : MonoBehaviour
     void Update()
     {
         int i = -1;
+        //測試用代碼
         if(IRS.TCP_Data != null) 
         {
             Data_name = IRS.TCP_Data;
@@ -40,6 +50,38 @@ public class Object_create_code : MonoBehaviour
         {
             Instantiate(create_Objs[i], create_pOS.transform);
             trunBT = false;
+        }
+        ////實際代碼
+        //if (trunBT = true)
+        //{
+        //    Data_name = IRS.TCP_Data;
+        //    trunBT = false ;
+        //}
+        //if (Data_name != null) 
+        //{
+        //    Status_Switch(Data_name);
+        //}
+    }
+    void Status_Switch(string data)
+    {
+        if (Enum.TryParse(data, true, out OrderStatus newState))
+        {
+            status = newState;
+            Debug.Log("狀態已改為：" + status);
+        }
+        else
+        {
+            Debug.LogWarning("無效的狀態：" + data);
+        }
+    }
+    void Status_List() 
+    {
+        switch (status) 
+        {
+            case OrderStatus.Obstacle:
+                /*要生成物件程式*/
+                Data_name = null;
+                break;
         }
     }
 }
