8581c9343909040268e43405207f505cf24becb6
romtyui <145352092+romtyui@users.noreply.github.com>
2025-05-08 18:08:10 +0800

    鏡頭控制

--- Assets/countpoint_code.cs
+++ Assets/countpoint_code.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class countpoint_code : MonoBehaviour
 {
     public int count_num ;
+    public Transform objtransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
