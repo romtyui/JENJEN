e3383cda2439b0fc098240d79d6691621bfb9478
romtyui <145352092+romtyui@users.noreply.github.com>
2025-06-02 01:55:12 +0800

    舌頭

--- Assets/No2_code.cs
+++ Assets/No2_code.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using static new_camera_contriol;
 
 public class No2_code : MonoBehaviour
 {
     public GameObject water;
+    public Animation snake;
+    public AnimationClip clip;
     public bool turn;
+    public float timer,t;
+    public new_camera_contriol NCC;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,8 +20,18 @@ public class No2_code : MonoBehaviour
     {
         if (turn)
         {
+            t += Time.deltaTime;
             water.SetActive(true);
-            turn = false;
+            if (t > timer) 
+            {
+                snake.clip = clip;
+                snake.Play();
+                turn = false;
+            }
         }
+    }
+    public void openmouth() 
+    {
+        NCC.status = Now_Scenes.snake2;
     }
 }
